724041ab714db5435988d4d23569a9aebf85ec94
annajennings <annajennings28@gmail.com>
2024-06-26 16:51:01 -0700

    rearrange

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // add bootstrap
 function add_bs_scripts_styles() {
 // wp_register_style('adobe-fonts', 'https://use.typekit.net/bmw4zvq.css');
-wp_register_style('bootstrap-style', get_template_directory_uri() . '/assets/css/index.css');
+wp_register_style('bootstrap-style', get_template_directory_uri() . '/librarium/dist/index.css');
  //empty array()for dependencies, version 0.1, place this in footer not head
-wp_register_script('bootstrap-script', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array(), true);
+wp_register_script('bootstrap-script', get_template_directory_uri() . '/librarium/dist/js/bootstrap.bundle.min.js', array(), true);
 // enqueue styles & scripts
 wp_enqueue_style('bootstrap-style');
 wp_enqueue_script('bootstrap-script');
