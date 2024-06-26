93a662cafb75644a1139d1af645718c88dff3df1
annajennings <annajennings28@gmail.com>
2024-06-25 22:40:28 -0700

    add adobe

--- functions.php
+++ functions.php
@@ -26,9 +26,10 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 15 );
 // add bootstrap
 function add_bs_scripts_styles() {
 // register styles & scripts
-wp_register_style('bootstrap-style', get_template_directory_uri() . '/assets/css/index.css');
+wp_register_style('adobe-fonts', 'https://use.typekit.net/bmw4zvq.css');
+wp_register_style('bootstrap-style', get_stylesheet_directory_uri() . '/assets/css/index.css');
  //empty array()for dependencies, version 0.1, place this in footer not head
-wp_register_script('bootstrap-script', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array(), '0.1', true);
+wp_register_script('bootstrap-script', get_stylesheet_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array(), '0.1', true);
 // enqueue styles & scripts
 wp_enqueue_style('bootstrap-style');
 wp_enqueue_script('bootstrap-script');
