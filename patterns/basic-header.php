04fcce871f7eb30153ea9653b4eb61bf3c6f7589
annajennings <annajennings28@gmail.com>
2024-06-26 16:39:12 -0700

    created index template, header&footer part/patterns

---
+++ patterns/basic-header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Basic header
+ * Slug: basic-header
+ */
+
+?>   
+
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|bg"},":hover":{"color":{"text":"var:preset|color|highlight"}}}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"contrast","textColor":"bg","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull has-bg-color has-contrast-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:columns {"verticalAlignment":null} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"bottom","width":"33.34%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"fontSize":"base"} /-->
+
+<!-- wp:site-tagline /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","width":"33.34%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":75,"shouldSyncIcon":true,"className":"is-style-rounded","style":{"color":[]}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","width":"33.33%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:33.33%"><!-- wp:navigation {"ref":301,"layout":{"type":"flex","justifyContent":"space-between"}} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
